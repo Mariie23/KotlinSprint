5fd170f8b71ca1330d3dada3ac740bdfa3490670
Mary <markoncher@gmail.com>
2025-08-02 01:54:33 +0300

    KS-1-2 imitation of logs

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksForPurchase: String = "Спасибо за покупку!"
+    println("Количество заказов: $numberOfOrders, текст с благодарностью: $thanksForPurchase")
+
+    var numberOfEmployees: Int = 2000
+    //println("Количество работников: $numberOfEmployees")
+    numberOfEmployees -= 1
+    println("Количество работников: $numberOfEmployees")
+}
