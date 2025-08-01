ba7717dff6a4b2dfc708b4350e87dce026d24aa1
Mary <markoncher@gmail.com>
2025-08-02 02:14:44 +0300

    KS-1-3 flight data output

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год: $year")
+    println("Час: $hour")
+    println("Минута: $minute")
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+}
