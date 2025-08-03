a16f963e5fc025cd7c621b5848f0e84f0338ba42
Mary <markoncher@gmail.com>
2025-08-04 02:21:33 +0300

    KS-2-1 changing data type numberOfStudents

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-    val numberOfStudents = 4.00
+    val numberOfStudents = 4
 
     val result1 = 3
     val result2 = 4
     val result3 = 3
     val result4 = 5
 
-    val arithmeticMean = ((result1 + result2 + result3 + result4) / numberOfStudents)
+    val arithmeticMean = ((result1 + result2 + result3 + result4) / 1.0 / numberOfStudents)
     println(arithmeticMean)
 
 }
