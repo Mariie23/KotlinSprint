2355de167ae1d9c85ac3ee57a2a56459bc3c8496
Mary <markoncher@gmail.com>
2025-08-04 02:04:28 +0300

    KS-1-3 change data type for time

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,13 +2,13 @@ package org.example.lesson_1
 
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println("Год: $year")
     println("Час: $hour")
     println("Минута: $minute")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print(hour)
     print(":")
     print(minute)
