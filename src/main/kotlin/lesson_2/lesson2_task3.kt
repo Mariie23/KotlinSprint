b0a0404749ce88b4af9e9f736510133a07739e0e
Mary <markoncher@gmail.com>
2025-08-05 02:42:26 +0300

    KS-2-3 adding const

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
-
+const val SECONDS_IN_MINUTE = 60
 fun main() {
     val hourDeparture: Int = 9
     val minutesDeparture: Int = 39
     val minutesOnTheWay: Int = 457
 
-    val departureInMinutes = hourDeparture * 60 + minutesDeparture
+    val departureInMinutes = hourDeparture * SECONDS_IN_MINUTE + minutesDeparture
     val arrivalInMinutes = departureInMinutes + minutesOnTheWay
-    val hourArrival = arrivalInMinutes / 60
-    val minutesArrival = arrivalInMinutes % 60
+    val hourArrival = arrivalInMinutes / SECONDS_IN_MINUTE
+    val minutesArrival = arrivalInMinutes % SECONDS_IN_MINUTE
     println("Время прибытия поезда: $hourArrival:$minutesArrival")
 
 
