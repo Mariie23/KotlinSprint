85ca145ed68505da2e548957cc59bb330d2e1b77
Mary <markoncher@gmail.com>
2025-08-05 03:16:29 +0300

    KS-3-1 adding greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Maria"
+    var welcome = "Good morning, $userName!"
+    println(welcome)
+    welcome = "Good night, $userName!"
+    println(welcome)
+
+}
