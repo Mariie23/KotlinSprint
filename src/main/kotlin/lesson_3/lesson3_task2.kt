bf20fe14c32ba3c76c0491dfef55461dc5dd3fa9
Mary <markoncher@gmail.com>
2025-08-05 03:24:39 +0300

    KS-3-2 changing personal data

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var name = "Татьяна"
+    var surname = "Андреева"
+    var patronymic = "Сергеевна"
+    var age = 20
+    println("$surname $name $patronymic, $age")
+
+    age = 22
+    surname = "Сидорова"
+    println("$surname $name $patronymic, $age")
+}
